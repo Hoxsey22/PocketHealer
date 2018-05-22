84193dc254eb0d42519bb9123cb58172d16951f1
Joseph Hoxsey <JosephHoxsey@gmail.com>
2018-05-21 23:57:39 -0700

    - Changed BossIcon to a ImageButton and added new skin for boss icon

--- core/src/com/hoxseygaming/pockethealer/BossIcon.java
+++ core/src/com/hoxseygaming/pockethealer/BossIcon.java
@@ -1,24 +1,23 @@
 package com.hoxseygaming.pockethealer;
 
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.hoxseygaming.pockethealer.encounters.entities.bosses.Boss;
 
 /**
  * Created by Hoxsey on 7/20/2017.
  */
 
-public class BossIcon extends Actor {
+public class BossIcon extends ImageButton {
 
     public Assets assets;
-    public Image image;
-    public Image glow;
-    public Image defeated;
+    //public Image image;
+    //public Image glow;
+    //public Image defeated;
     public Boss boss;
     public String description;
-    public boolean isSelected;
+    //public boolean isSelected;
 
+    /*
     public BossIcon(Assets assets)   {
         setName("Boss Location");
         this.assets = assets;
@@ -27,43 +26,45 @@ public class BossIcon extends Actor {
         defeated = new Image(assets.getTexture(assets.defeatedBossIcon));
         isSelected = false;
     }
+    */
 
     public BossIcon(Assets assets, Boss boss)   {
+        super(assets.getSkin(),boss.getBossIconStyle());
+
         this.assets = assets;
         this.boss = boss;
 
         setName(boss.name);
+
+
+
         description = boss.getDescription();
 
-        image = new Image(assets.getTexture(assets.bossLocation));
-        image.setBounds(assets.bossIconPosition.get(boss.id-2).x,assets.bossIconPosition.get(boss.id-2).y, 50, 50);
+        //image = new Image(assets.getTexture(assets.bossLocation));
 
-        glow = new Image(assets.getTexture(assets.selectedLevel));
-        glow.setBounds(image.getX(),image.getY(),image.getWidth(), image.getHeight());
+        setBounds(assets.bossIconPosition.get(boss.id-2).x, assets.bossIconPosition.get(boss.id-2).y, 50, 50);
 
-        defeated = new Image(assets.getTexture(assets.defeatedBossIcon));
-        defeated.setBounds(image.getX(),image.getY(),image.getWidth(), image.getHeight());
 
-        setBounds(image.getX(), image.getY(), image.getWidth(), image.getHeight());
 
-        System.out.println(this.getDebug());
+
+        //glow = new Image(assets.getTexture(assets.selectedLevel));
+        //glow.setBounds(image.getX(),image.getY(),image.getWidth(), image.getHeight());
+
+        //defeated = new Image(assets.getTexture(assets.defeatedBossIcon));
+        //defeated.setBounds(image.getX(),image.getY(),image.getWidth(), image.getHeight());
+
+        //setBounds(image.getX(), image.getY(), image.getWidth(), image.getHeight());
+
+        //System.out.println(this.getDebug());
     }
 
-    @Override
-    public void setPosition(float x, float y) {
-        super.setPosition(x, y);
-        image.setPosition(x,y);
-        glow.setPosition(x,y);
-        defeated.setPosition(x,y);
-    }
+    //public void select()    {
+    //    isSelected = true;
+    //}
 
-    public void select()    {
-        isSelected = true;
-    }
-
-    public void unselect() {
-        isSelected = false;
-    }
+    //public void unselect() {
+     //   isSelected = false;
+   // }
 
     public String getDescription()  {
         return description;
@@ -73,6 +74,7 @@ public class BossIcon extends Actor {
         return boss;
     }
 
+    /*
     @Override
     public void draw(Batch batch, float parentAlpha) {
         if(boss.isDefeated())    {
@@ -85,5 +87,5 @@ public class BossIcon extends Actor {
         else
             image.draw(batch, parentAlpha);
 
-    }
+    }*/
 }
