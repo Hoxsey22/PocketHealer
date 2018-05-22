510089792246b2fd904c4b36e43fb074d7f502c2
Joseph Hoxsey <JosephHoxsey@gmail.com>
2018-05-22 00:03:59 -0700

    - Added TextButtons to the mapstate to add changeListener - Add method to create the buttons and the listeners for each

--- core/src/com/hoxseygaming/pockethealer/states/MapState.java
+++ core/src/com/hoxseygaming/pockethealer/states/MapState.java
@@ -7,12 +7,16 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.hoxseygaming.pockethealer.Assets;
 import com.hoxseygaming.pockethealer.AudioManager;
 import com.hoxseygaming.pockethealer.BossIcon;
-import com.hoxseygaming.pockethealer.Button;
-import com.hoxseygaming.pockethealer.ImageButton;
+import com.hoxseygaming.pockethealer.InfoFrame;
 import com.hoxseygaming.pockethealer.MapFrame;
 import com.hoxseygaming.pockethealer.Player;
 
@@ -28,22 +32,41 @@ public class MapState extends State {
     public Assets assets;
     public MapFrame mapFrame;
     public BossIcon selectedLevel;
+    InfoFrame infoFrame;
 
     public ImageButton pageLeft;
     public ImageButton pageRight;
+    private TextButton infoButton;
+    private TextButton talentButton;
+    private TextButton startButton;
+    private TextButton spellButton;
+    private Table buttonTable;
 
 
     public MapState(StateManager sm, Player player) {
         super(sm);
 
         stage = new Stage(viewport);
+
+        Gdx.input.setInputProcessor(stage);
+
         assets = player.getAssets();
+
+        infoFrame = new InfoFrame(assets);
 
         AudioManager.playMusic(assets.getMusic(assets.mmMusic), true);
 
-        pageLeft = new ImageButton("pageLeft", assets.getTexture(assets.pageTurn), 40, 550, 30,30);
+        pageLeft = new ImageButton(assets.getSkin(), "page_left");
+        pageLeft.setBounds(40, 550, 30,30);
+
+        pageRight = new ImageButton(assets.getSkin(), "page_right");
+        pageRight.setBounds(410, 550, 30,30);
+
+        /*
+        pageLeft = new ImageButton("pageLeft", assets.getTexture(assets.pageRight), 40, 550, 30,30);
         pageLeft.flipX();
-        pageRight = new ImageButton("pageRight", assets.getTexture(assets.pageTurn), 410, 550, 30,30);
+        pageRight = new ImageButton("pageRight", assets.getTexture(assets.pageRight), 410, 550, 30,30);
+        */
 
         page = 1;
         maxPage = 1;
@@ -52,6 +75,8 @@ public class MapState extends State {
 
         //turnPage();
         startPage();
+        createButtons();
+        createButtonListeners();
     }
 
     public void turnPage()  {
@@ -67,7 +92,7 @@ public class MapState extends State {
                 stage.addActor(mapFrame);
 
                 if(player.getLevel() > 6) {
-                    pageRight.addToStage(stage);
+                    stage.addActor(pageRight);
                     //stage.addActor(pageRight);
                 }
                 break;
@@ -75,25 +100,24 @@ public class MapState extends State {
                 stage.addActor(mapFrame);
 
                 if(player.getLevel() > 11) {
-                    pageRight.addToStage(stage);
-                    //stage.addActor(pageRight);
+                    //pageRight.addToStage(stage);
+                    stage.addActor(pageRight);
                 }
-                pageLeft.addToStage(stage);
-                //stage.addActor(pageLeft);
+                //pageLeft.addToStage(stage);
+                stage.addActor(pageLeft);
                 break;
             case 3:
                 stage.addActor(mapFrame);
-                pageLeft.addToStage(stage);
-                //stage.addActor(pageLeft);
+                //pageLeft.addToStage(stage);
+                stage.addActor(pageLeft);
                 break;
         }
         loadPage();
+        createButtons();
+
     }
 
     public void startPage()  {
-
-        pageLeft.remove();
-        pageRight.remove();
 
         if(player.getLevel() <= 6)
             mapFrame = new MapFrame(player, 1, assets);
@@ -101,6 +125,11 @@ public class MapState extends State {
             mapFrame = new MapFrame(player, 2, assets);
         else
             mapFrame = new MapFrame(player, 3, assets);
+
+
+        pageLeft.remove();
+        pageRight.remove();
+
         page = mapFrame.page;
         //mapFrame = new MapFrame(player, page, assets);
         mapFrame.remove();
@@ -110,27 +139,28 @@ public class MapState extends State {
                 stage.addActor(mapFrame);
 
                 if(player.getLevel() > 6) {
-                    pageRight.addToStage(stage);
-                    //stage.addActor(pageRight);
+                    //pageRight.addToStage(stage);
+                    stage.addActor(pageRight);
                 }
                 break;
             case 2:
                 stage.addActor(mapFrame);
 
                 if(player.getLevel() > 11) {
-                    pageRight.addToStage(stage);
-                    //stage.addActor(pageRight);
+                    //pageRight.addToStage(stage);
+                    stage.addActor(pageRight);
                 }
-                pageLeft.addToStage(stage);
-                //stage.addActor(pageLeft);
+                //pageLeft.addToStage(stage);
+                stage.addActor(pageLeft);
                 break;
             case 3:
                 stage.addActor(mapFrame);
-                pageLeft.addToStage(stage);
-                //stage.addActor(pageLeft);
+                //pageLeft.addToStage(stage);
+                stage.addActor(pageLeft);
                 break;
         }
         loadPage();
+        createBossIconListeners();
     }
 
     public void loadPage()  {
@@ -165,7 +195,7 @@ public class MapState extends State {
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
                 Vector2 coord = stage.screenToStageCoordinates(new Vector2((float) screenX, (float) screenY));
-
+                /*
                 if(coord.y > mapFrame.innerFrame.getY())    {
 
                     if(pageLeft.pressed(coord.x, coord.y))    {
@@ -192,11 +222,12 @@ public class MapState extends State {
                         mapFrame.setTitle(bi.getName());
                         mapFrame.setBody(bi.getDescription());
                         bi.select();
-                        mapFrame.startButton.setHighlight(true);
+//                        mapFrame.startButton.setChecked(true);
                     }
-                }
-                if(coord.y < mapFrame.talentButton.getY() + mapFrame.talentButton.getHeight())    {
-                    Button hit = mapFrame.hitButton(coord.x, coord.y);
+                }*/
+                /*
+                if(coord.y < mapFrame.infoButton.getY() + mapFrame.talentButton.getHeight())    {
+                    Actor hit = mapFrame.hit(coord.x, coord.y, false);
                     if(hit != null)    {
                         switch (hit.getName())   {
                             case "TALENTS":
@@ -210,10 +241,14 @@ public class MapState extends State {
                             case "SPELLS":
                                 sm.set(new SpellSelectionState(sm, player));
                                 break;
+                            case "INFO":
+                                stage.addActor(infoFrame);
+                                break;
                         }
 
                     }
                 }
+                */
                 return false;
             }
 
@@ -239,9 +274,105 @@ public class MapState extends State {
         });
     }
 
+    public void createButtons() {
+        if(buttonTable != null) {
+            buttonTable.remove();
+        }
+        else {
+            buttonTable = new Table();
+            buttonTable.setBounds(mapFrame.innerFrame.getX(), 20, mapFrame.innerFrame.getWidth(), mapFrame.innerFrame.getY()-15);
+            //buttonTable.debug();
+
+            infoButton = new TextButton("INFO", assets.getSkin(), "small_button");
+            infoButton.setName("INFO");
+
+            talentButton = new TextButton("TALENT", assets.getSkin());
+
+            startButton = new TextButton("START", assets.getSkin());
+
+            spellButton = new TextButton("SPELL", assets.getSkin());
+
+            buttonTable.add(talentButton).padRight(5).padTop(5);
+            buttonTable.add(startButton).padRight(5).padLeft(5).padTop(5);
+            buttonTable.add(spellButton).padLeft(5).padTop(5);
+        }
+
+        if(player.getTalentTree().getUnusedPoints() > 1)    {
+            talentButton.setChecked(true);
+        } else {
+            talentButton.setChecked(false);
+        }
+
+
+
+        stage.addActor(buttonTable);
+    }
+
+    public void createButtonListeners()   {
+
+        talentButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                sm.set(new TalentSelectionState(sm, player));
+            }
+        });
+
+        startButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if(selectedLevel != null)    {
+                    sm.set(new EncounterState(sm, player, selectedLevel.boss));
+                }
+            }
+        });
+
+        spellButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                sm.set(new SpellSelectionState(sm, player));
+            }
+        });
+
+        pageLeft.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                page--;
+                turnPage();
+                System.out.println("Page Left");
+            }
+        });
+
+        pageRight.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                page++;
+                turnPage();
+                System.out.println("Page Right");
+            }
+        });
+    }
+
+    public void createBossIconListeners()   {
+        for(int i = 0; i < mapFrame.bossIconsList.size(); i++)   {
+            mapFrame.bossIconsList.get(i).addListener(new ChangeListener() {
+
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    if(selectedLevel != null)    {
+                        selectedLevel.setChecked(false);
+                    }
+                    selectedLevel = (BossIcon) actor;
+                    mapFrame.setTitle(selectedLevel.getName());
+                    mapFrame.setBody(selectedLevel.getDescription());
+                    //selectedLevel.setChecked(true);
+                }
+            });
+        }
+    }
+
     @Override
     public void update(float dt) {
-        handleInput();
+        //handleInput();
     }
 
     @Override
